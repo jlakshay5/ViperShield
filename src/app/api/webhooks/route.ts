144fd82092627514d421f03fad3e1f1519ec4357
Lakshay Jain <lakke21@Lakshays-MacBook-Air.local>
2024-10-07 16:16:32 +0530

    initial commit

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
   try {
     const body = await req.text();
 
-    const signature = headers().get("stripes/signature");
+    const signature = headers().get("stripe-signature");
 
     if (!signature) {
       return new Response("Invalid signature", { status: 400 });
