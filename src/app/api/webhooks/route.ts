127be9c79fae2ffd082ccf81fd0509780aee724e
Lakshay Jain <lakke21@Lakshays-MacBook-Air.local>
2024-10-09 19:16:48 +0530

    update

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -72,14 +72,14 @@ export async function POST(req: Request) {
       });
 
       await resend.emails.send({
-        from: "ViperShield <jainlakshay21@gmail.com>",
+        from: "CaseCobra <jainlakshay21@gmail.com>",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order!",
         react: OrderReceivedEmail({
           orderId,
           orderDate: updatedOrder.createdAt.toLocaleDateString(),
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-expect-error
+          // @ts-ignore
           shippingAddress: {
             name: session.customer_details!.name!,
             city: shippingAddress!.city!,
