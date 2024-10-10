5840e16f2d76ccefee1d3a5894a893bcb77e3d4a
Lakshay Jain <lakke21@Lakshays-MacBook-Air.local>
2024-10-10 10:14:50 +0530

    update

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
       });
 
       await resend.emails.send({
-        from: "ViperShield <lakshaycodes.online>",
+        from: "ViperShield <support@lakshaycodes.online>",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order!",
         react: OrderReceivedEmail({
